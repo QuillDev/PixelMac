4b1c8f8fbce9c047324600be5689cb35565920ff
Robert Brunson <siinspam@gmail.com>
2023-12-21 17:12:35 -0500

    woo

--- compile.js
+++ compile.js
@@ -1,15 +1,21 @@
 import { readFile, writeFile } from "fs/promises"
 
+const paths = [
+    "quill_earth_des"
+];
+
 (async () => {
-    let [raw, template] = await Promise.all([
-        readFile("quills_earth_raw.xml", { encoding: "utf-8" }),
-        readFile("quills_earth_template.xml", { encoding: "utf-8" })
-    ]);
+    for (const path of paths) {
+        let [raw, template] = await Promise.all([
+            readFile("custom/" + path + ".xml.raw", { encoding: "utf-8" }),
+            readFile("custom/" + path + ".xml.template", { encoding: "utf-8" })
+        ]);
 
-    if (raw.charCodeAt(0) === 0xFEFF) {
-        raw = raw.substring(1);
+        if (raw.charCodeAt(0) === 0xFEFF) {
+            raw = raw.substring(1);
+        }
+
+        const compiled = template.replace("<TEMPLATE_MARKER>", raw)
+        await writeFile(path + ".xml", compiled, { encoding: "utf-8" });
     }
-
-    const compiled = template.replace("<TEMPLATE_MARKER>", raw)
-    await writeFile("quills_earth.xml", compiled, { encoding: "utf-8" });
 })();
